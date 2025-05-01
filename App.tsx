e13f005b26d4baaffc01b8fa47cebd183463f77e
vxnshK <vkapoor0112@gmail.com>
2025-05-01 22:35:48 +0530

    fixes for ripple

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
+import {TouchableWithoutFeedback, View} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,15 +20,19 @@ function App() {
               fontSize: 14,
               fontWeight: '600',
               fontFamily: 'Gotham Medium',
-              marginTop: 15
+              marginTop: 15,
             },
             tabBarActiveTintColor: '#007aff',
             tabBarInactiveTintColor: 'gray',
             tabBarIconStyle: {
-              display: 'none'
-            }
-          }}
-        >
+              display: 'none',
+            },
+            tabBarButton: props => (
+              <TouchableWithoutFeedback onPress={props.onPress}>
+                <View style={{flex: 1}}>{props.children}</View>
+              </TouchableWithoutFeedback>
+            ),
+          }}>
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>

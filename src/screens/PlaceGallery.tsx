f0fa74058dfbb8858df58364b52e68a784bc1ef3
vxnshK <vkapoor0112@gmail.com>
2025-05-07 23:06:38 +0530

    init metadata ui

--- src/screens/PlaceGallery.tsx
+++ src/screens/PlaceGallery.tsx
@@ -1,53 +1,116 @@
 import React from 'react';
 import {StyleSheet, View, Text, Image} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+
 import {SafeAreaView} from 'react-native-safe-area-context';
+
+import {ScrollView} from 'react-native-gesture-handler';
+
+import Ionicons from '@react-native-vector-icons/ionicons';
 
 export default function PlaceGallery({route}: any) {
   const {item} = route.params;
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.placeGalleryContainer}>
       <Image source={{uri: item.gallery[0]}} style={styles.image} />
       <ScrollView style={styles.dataContainer}>
-        <Text style={styles.city}>{item.city}</Text>
-        <View style={styles.countryFlagContainer}>
-          <Text>{item.flag}</Text>
-          <Text style={styles.country}>{item.country}</Text>
+        
+        <View style={styles.ratingCityContainer}>
+          <Text style={styles.city}>{item.city}</Text>
+          <View style={styles.ratingsContainer}>
+            <Ionicons name="star-sharp" size={16} color={'#f29d0a'} />
+            <Text style={styles.ratings}>{item.ratings}</Text>
+          </View>
         </View>
+
+        <View style={styles.countryFlagReviewsCountContainer}>
+          <View style={styles.countryFlagContainer}>
+            <Text>{item.flag}</Text>
+            <Text style={styles.country}>{item.country}</Text>
+          </View>
+          <Text style={styles.reviewCount}>{item.reviewCount} reviews</Text>
+        </View>
+
+        <Text style={styles.description}>{item.description}</Text>
+
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  placeGalleryContainer: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
   image: {
-    height: 300,
+    height: 450,
     width: '100%',
     position: 'absolute',
     top: -100,
   },
   dataContainer: {
     position: 'absolute',
-    top: 100,
+    top: 250,
     width: '100%',
     backgroundColor: '#ffffff',
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    borderRadius: 100,
-    gap: 10,
+    paddingHorizontal: 28,
+    paddingVertical: 24,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  ratingCityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
   },
   city: {
     fontFamily: 'Gotham Bold',
     fontSize: 20,
-    marginBottom: 5,
+  },
+  ratingsContainer: {
+    borderWidth: 1,
+    borderColor: '#7c8891',
+    borderRadius: 100,
+    paddingHorizontal: 10,
+    paddingTop: 4,
+    paddingBottom: 4.4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+  ratings: {
+    fontFamily: 'Gotham Book',
+    fontSize: 12,
+  },
+  countryFlagReviewsCountContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 26,
   },
   countryFlagContainer: {
     flexDirection: 'row',
     gap: 5,
+    alignItems: 'center',
   },
   country: {
-    fontFamily: 'Gotham Book',
+    fontFamily: 'Gotham Medium',
     fontSize: 15,
   },
+  reviewCount: {
+    fontFamily: 'Gotham Medium',
+    fontSize: 13,
+    borderBottomWidth: 0.8,
+    borderBottomColor: '#212528',
+    paddingBottom: 2.4,
+    paddingHorizontal: 1,
+  },
+  description: {
+    fontFamily: 'Gotham Book',
+    fontSize: 16,
+    lineHeight: 18,
+
+  }
 });
